a23081d6e1d40ef75fc53b7cbb27267b80a67ea6
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-03-27 17:35:29 +0000

    Fix Betty line length in postorder function

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - post-order traversal of a binary tree
  * @tree: pointer to the root node
- * @func: pointer to function to call for each node
+ * @func: function to call for each node
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
