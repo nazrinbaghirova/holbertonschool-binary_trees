fd39cc96c7d59ccf8490965af1d74e374358dc93
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-03-27 16:41:26 +0000

    Add binary_tree_insert_left function

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
