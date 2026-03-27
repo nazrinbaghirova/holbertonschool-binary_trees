073ef5794f7192605e1e6f6229b7d000b1ed3d15
nazrinbaghirova <nbaghirova20025@ada.edu.az>
2026-03-27 18:15:54 +0000

    Add binary_tree_nodes function

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
